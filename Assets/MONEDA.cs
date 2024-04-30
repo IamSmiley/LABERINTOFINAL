6115eca2c55e46af7588a2c7b5a9cc7805af2d32
Tomasmora01 <nicolass0129@gmail.com>
2024-04-30 11:41:20 -0500

    FINALAHORASI

--- Assets/MONEDA.cs
+++ Assets/MONEDA.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CoinController : MonoBehaviour
 {
-    public float timeToAdd = 5f; 
+    public float timeToAdd = 10f; 
 
     private void OnTriggerEnter(Collider other)
     {
